19134a8fee251f8551777c89e1b5302257c486ef
DenDD-Png <den.drukoff@yandex.ru>
2025-07-01 21:57:40 +0300

    Добавил новый тест

--- demoqa_test/model/conftest.py
+++ demoqa_test/model/conftest.py
@@ -4,28 +4,18 @@ from selenium.webdriver.chrome.options import Options
 from selene import Browser, Config
 
 
-
-
-@pytest.fixture(scope="function", autouse=True)
-def open_browser(setup_browser):
-    browser = setup_browser
-    browser.open('https://demoqa.com/automation-practice-form')
-    yield
-    browser.quit()
-
 @pytest.fixture(scope='function')
-def setup_browser():
+def setup_browser(request):
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
         "browserVersion": "127.0",
         "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True
+            "enableVNC": False,
+            "enableVideo": False
         }
     }
     options.capabilities.update(selenoid_capabilities)
-
     driver = webdriver.Remote(
         command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
         options=options
@@ -33,4 +23,11 @@ def setup_browser():
 
     browser = Browser(Config(driver))
     yield browser
+
+
+@pytest.fixture(scope="function", autouse=True)
+def open_browser(setup_browser):
+    browser = setup_browser
+    browser.open('https://demoqa.com/automation-practice-form')
+    yield
     browser.quit()
